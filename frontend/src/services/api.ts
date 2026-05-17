1bc0b82897274f069585131a8b800dbdb2389b0d
manishjk08 <jamarkattelmanish2@gmail.com>
2026-05-17 14:23:01 +0545

    changes in url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,7 +7,7 @@ import { store } from "../store";
 let refreshPromise: Promise<string> | null = null;
 
 const api = axios.create({
-  baseURL: "https://bank-management-system-2-cchz.onrender.com/",
+  baseURL: "https://bank-management-system-2-cchz.onrender.com/api",
   withCredentials: true,
 });
 

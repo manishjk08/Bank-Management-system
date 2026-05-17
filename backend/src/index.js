f92f4cae32180e01629d8756e82f6f8ea107cf87
manishjk08 <jamarkattelmanish2@gmail.com>
2026-05-17 14:24:50 +0545

    make chnages in url

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const PORT=process.env.PORT || 5000;
 // Security & middleware
 app.use(helmet());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://bank-management-system-95q8.vercel.app/",
   credentials:true
 }));
 app.use(morgan('dev'));
